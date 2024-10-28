47984a2081cc3d78cdb7a802e52f3b34685d7144
Nheya <nheyakumar@gmail.com>
2024-10-28 15:56:34 -0400

    Initial commit

--- CSC120_LAB/Lab9/src/Family.java
+++ CSC120_LAB/Lab9/src/Family.java
@@ -1,5 +1,30 @@
 //addPerson, getNumberOfPeople, getTotalAge, display, birthday
 
+import java.util.Scanner;
 
+//----------------------------------------------------------------------------------------------------------------------
 public class Family {
+
+    private static final int MAX_FAMILY_MEMBERS = 10;
+
+    public static void addPerson () {
+
+    }
+
+    public static void getNumberOfPeople () {
+
+    }
+
+    public static void getTotalAge () {
+
+    }
+
+    public static void display () {
+
+    }
+
+    public static void birthday() {
+
+    }
+
 }
